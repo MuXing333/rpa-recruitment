5341cb9000220f9cb3645f2d7ecb0888b868e319
MuXing333 <mu.xing@griffithuni.edu.au>
2019-03-12 20:00:16 +0800

    done

--- php/test/MyGreeter_Client_Test.php
+++ php/test/MyGreeter_Client_Test.php
@@ -20,5 +20,14 @@ class MyGreeter_Client_Test extends \PHPUnit_Framework_TestCase
         $this->assertTrue(
             strlen($this->greeter->getGreeting()) > 0
         );
+        $this->asserTrue(
+            $this->greeter->getGreeting('12:01:00') == "Good morning"
+        );
+        $this->asserTrue(
+            $this->greeter->getGreeting('17:59:00') == "Good afternoon"
+        );
+        $this->asserTrue(
+            $this->greeter->getGreeting('23:59:59') == "Good evening"
+        );
     }
 }
